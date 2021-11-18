3c1fc5f23ab27c89e276dd980958f5f55d8b0df7
mmodras <mmodras@gmail.com>
2021-11-18 18:21:03 -0500

    Adding audio files + html, css, js changes

--- soundboard.js
+++ soundboard.js
@@ -1,3 +1,17 @@
 window.onload = function() {
+    function play1() {
+        var audio = document.getElementById("audio1");
+        audio.play();
+      }
+    
+      function play2() {
+        var audio = document.getElementById("audio2");
+        audio.play();
+      }
+    
+      function play3() {
+        var audio = document.getElementById("audio3");
+        audio.play();
+      }
 
 };
